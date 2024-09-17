fcc2a50bcaa5feb2c091856f72a6b3de1f2eee7b
Sinca Vlad <42393928+SincaVlad@users.noreply.github.com>
2024-09-17 16:48:48 +0300

    Update 0.4_model_usage.py

--- scripts/0.4_model_usage.py
+++ scripts/0.4_model_usage.py
@@ -1,11 +1,10 @@
 import joblib
-import os
 
-# Încărcarea modelului salvat din directorul specificat
-model_path = os.path.join('results', 'model_optimization_results', 'Optimized_RandomForest_Model.joblib')
+# Loading the saved model from the specified directory
+model_path = 'results/0.3_optimized_model/Optimized_RandomForest_Model.joblib'
 best_rf = joblib.load(model_path)
 
-# Definirea unei noi compoziții ca dicționar
+# Defining a new composition as a dictionary
 tested_composition = {
     "BaO": 0,
     "CaO": 0,
@@ -28,25 +27,25 @@ tested_composition = {
     "P2O5": 0
 }
 
-# Verificarea dacă suma compozițiilor este egală cu 100%
+# Checking if the sum of compositions is equal to 100%
 total_composition = sum(tested_composition.values())
 
 if total_composition != 100:
     raise ValueError(f"Suma compozițiilor trebuie să fie egală cu 100%. Suma actuală este {total_composition:.2f}%")
     
-# Pregătirea compoziției pentru predicție - doar valorile
+# Preparing the composition for prediction - only values
 composition_values = list(tested_composition.values())
 
-# Predicția Tg pentru noua compoziție
+# Predicting Tg for the new composition
 predicted_tg = best_rf.predict([composition_values])
 
-# Filtrarea și formatarea componentelor care nu sunt 0
+# Filtering and formatting components that are not 0
 composition_str = ", ".join([
     f"{component}: {amount}%" 
     for component, amount in tested_composition.items() 
     if amount != 0
 ])
 
-# Afișarea compoziției care nu conține 0 și a Tg prezis
+# Displaying the composition that does not contain 0 and the predicted Tg
 print(f"Compoziție: {composition_str}")
 print(f"Predicted Tg: {predicted_tg[0]:.2f}")
